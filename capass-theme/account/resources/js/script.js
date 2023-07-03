b6e22f2e0b3528203469df8ed75d534de3dccb5c
kotake_satoshi <kotake_satoshi@cyberagent.co.jp>
2023-07-04 00:59:33 +0900

    test

--- capass-theme/account/resources/js/script.js
+++ capass-theme/account/resources/js/script.js
@@ -1,16 +1,1 @@
-document.body.onload = addElement;
-
-function addElement () {
-  // 新しい div 要素を作成します
-  const newDiv = document.createElement("div");
-
-  // いくつかの内容を与えます
-  const newContent = document.createTextNode("Hi there and greetings!");
-
-  // テキストノードを新規作成した div に追加します
-  newDiv.appendChild(newContent);
-
-  // DOM に新しく作られた要素とその内容を追加します
-  const currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
-}
+console.log("CA PASS")
